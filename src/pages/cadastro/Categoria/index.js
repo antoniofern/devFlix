168cab0eb004f9fb1c3b733c9d46a5584dcf0eba
antoniofern <antoniofern.n@gmail.com>
2020-07-28 14:52:23 -0300

    adding react-slick and some styles in carousel

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,6 +6,15 @@ export default function CadastroCategoria() {
   return (
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
+
+      <form>
+        <label>
+          Nome da categoria:
+          <input type='text' />
+        </label>
+        <button>Cadastrar</button>
+      </form>
+
       <Link to='/'>Ir para home</Link>
     </PageDefault>
   );
