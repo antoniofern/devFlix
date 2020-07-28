5bb6fe43e94c241bb64a5836b2da5fd9def35f93
antoniofern <antoniofern.n@gmail.com>
2020-07-28 00:04:11 -0300

    adding components and styles

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import logo from '../../assets/img/logo.png';
+import './styles.css';
+import ButtonLink from '../components/ButtonLink';
 
 export default function Menu() {
-  return <nav>devFlix</nav>;
+  return (
+    <nav className='Menu'>
+      <a href='/'>
+        <img className='Logo' src={logo} alt='DEVFLIX logo' />
+      </a>
+      <ButtonLink className='ButtonLink' href='/'>
+        Novo vídeo
+      </ButtonLink>
+    </nav>
+  );
 }
